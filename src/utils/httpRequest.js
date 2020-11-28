e369426200801e197a3b3c47180f8f2563d527a6
刘辉 <hui.liu@rivo-robot.com>
2020-11-28 09:22:48 +0800

    abc

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -1,49 +1,60 @@
-import Vue from 'vue'
-import axios from 'axios'
-import router from '@/router'
-import qs from 'qs'
-import merge from 'lodash/merge'
-import { clearLoginInfo } from '@/utils'
+import Vue from "vue";
+import axios from "axios";
+import router from "@/router";
+import qs from "qs";
+import merge from "lodash/merge";
+import { clearLoginInfo } from "@/utils";
 
 const http = axios.create({
   timeout: 1000 * 30,
   withCredentials: true,
   headers: {
-    'Content-Type': 'application/json; charset=utf-8'
+    "Content-Type": "application/json; charset=utf-8"
   }
-})
+});
 
 /**
  * 请求拦截
  */
-http.interceptors.request.use(config => {
-  config.headers['token'] = Vue.cookie.get('token') // 请求头带上token
-  return config
-}, error => {
-  return Promise.reject(error)
-})
+http.interceptors.request.use(
+  config => {
+    config.headers["token"] = Vue.cookie.get("token"); // 请求头带上token
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 /**
  * 响应拦截
  */
-http.interceptors.response.use(response => {
-  if (response.data && response.data.code === 401) { // 401, token失效
-    clearLoginInfo()
-    router.push({ name: 'login' })
+http.interceptors.response.use(
+  response => {
+    if (response.data && response.data.code === 401) {
+      // 401, token失效
+      clearLoginInfo();
+      router.push({ name: "login" });
+    }
+    return response;
+  },
+  error => {
+    return Promise.reject(error);
   }
-  return response
-}, error => {
-  return Promise.reject(error)
-})
+);
 
 /**
  * 请求地址处理
  * @param {*} actionName action方法名称
  */
-http.adornUrl = (actionName) => {
+http.adornUrl = actionName => {
   // 非生产环境 && 开启代理, 接口前缀统一使用[/proxyApi/]前缀做代理拦截!
-  return (process.env.NODE_ENV !== 'production' && process.env.OPEN_PROXY ? '/proxyApi/' : window.SITE_CONFIG.baseUrl) + actionName
-}
+  return (
+    (process.env.NODE_ENV !== "production" && process.env.OPEN_PROXY
+      ? "/proxyApi/"
+      : window.SITE_CONFIG.baseUrl) + actionName
+  );
+};
 
 /**
  * get请求参数处理
@@ -52,10 +63,10 @@ http.adornUrl = (actionName) => {
  */
 http.adornParams = (params = {}, openDefultParams = true) => {
   var defaults = {
-    't': new Date().getTime()
-  }
-  return openDefultParams ? merge(defaults, params) : params
-}
+    t: new Date().getTime()
+  };
+  return openDefultParams ? merge(defaults, params) : params;
+};
 
 /**
  * post请求数据处理
@@ -65,12 +76,79 @@ http.adornParams = (params = {}, openDefultParams = true) => {
  *  json: 'application/json; charset=utf-8'
  *  form: 'application/x-www-form-urlencoded; charset=utf-8'
  */
-http.adornData = (data = {}, openDefultdata = true, contentType = 'json') => {
+http.adornData = (data = {}, openDefultdata = true, contentType = "json") => {
   var defaults = {
-    't': new Date().getTime()
-  }
-  data = openDefultdata ? merge(defaults, data) : data
-  return contentType === 'json' ? JSON.stringify(data) : qs.stringify(data)
-}
+    t: new Date().getTime()
+  };
+  data = openDefultdata ? merge(defaults, data) : data;
+  return contentType === "json" ? JSON.stringify(data) : qs.stringify(data);
+};
 
-export default http
+http.get = (action, params = {}, openDefultParams = true) => {
+  let url = http.adornUrl(action);
+  let success = params.success;
+  let error = params.error;
+  delete params.success;
+  delete params.error;
+  let prms = http.adornParams(params, openDefultParams);
+
+  http({
+    url: url,
+    params: prms,
+    method: "get"
+  }).then(
+    ({ data }) => {
+      if (data && data.code === 0) {
+        if (success) {
+          success(data);
+        }
+      } else {
+        console.log(data.msg);
+        if (error) {
+          error(data);
+        }
+      }
+    },
+    ({ err }) => {
+      console.log(err);
+    }
+  );
+};
+
+http.post = (
+  action = "",
+  data = {},
+  openDefultdata = true,
+  contentType = "json"
+) => {
+  let url = http.adornUrl(action);
+  let success = data.success;
+  let error = data.error;
+  delete data.success;
+  delete data.error;
+  let body = http.adornData(data, openDefultdata, contentType);
+
+  http({
+    url: url,
+    data: body,
+    method: "post"
+  }).then(
+    ({ data }) => {
+      if (data && data.code === 0) {
+        if (success) {
+          success(data);
+        }
+      } else {
+        console.log(data.msg);
+        if (error) {
+          error(data);
+        }
+      }
+    },
+    ({ err }) => {
+      console.log(err);
+    }
+  );
+};
+
+export default http;
---
+++ src/views/modules/busi/busihouselayout-add-or-update.vue
@@ -0,0 +1,237 @@
+<template>
+  <el-dialog
+    :title="!dataForm.id ? '新增' : '修改'"
+    :close-on-click-modal="false"
+    :visible.sync="visible"
+  >
+    <el-form
+      :model="dataForm"
+      :rules="dataRule"
+      ref="dataForm"
+      @keyup.enter.native="dataFormSubmit()"
+      label-width="80px"
+    >
+      <el-form-item label="项目" prop="projectName">
+        <el-autocomplete
+          popper-class="my-autocomplete"
+          v-model="dataForm.projectName"
+          :fetch-suggestions="listProject"
+          placeholder="请输入项目名称"
+          @select="handleSelect"
+        >
+          <i
+            class="el-icon-edit el-input__icon"
+            slot="suffix"
+            @click="handleIconClick"
+          >
+          </i>
+          <template slot-scope="{ item }">
+            <div class="name">{{ item.id }}</div>
+            <span class="addr">{{ item.name }}</span>
+          </template>
+        </el-autocomplete>
+      </el-form-item>
+      <el-form-item label="图片名称" prop="name">
+        <el-input v-model="dataForm.name" placeholder="图片名称"></el-input>
+      </el-form-item>
+      <el-form-item label="图片说明" prop="memo">
+        <el-input v-model="dataForm.memo" placeholder="图片说明"></el-input>
+      </el-form-item>
+      <el-form-item label="图片" prop="path">
+        <el-upload
+          :headers="headers"
+          :data="data"
+          :action="uploadUrl"
+          list-type="picture-card"
+          :auto-upload="true"
+          :limit="1"
+          :on-success="hanldeSuccess"
+        >
+          <i slot="default" class="el-icon-plus"></i>
+          <div slot="file" slot-scope="{ file }">
+            <img
+              class="el-upload-list__item-thumbnail"
+              :src="file.url"
+              alt=""
+            />
+            <span class="el-upload-list__item-actions">
+              <span
+                class="el-upload-list__item-preview"
+                @click="handlePictureCardPreview(file)"
+              >
+                <i class="el-icon-zoom-in"></i>
+              </span>
+              <span
+                v-if="!disabled"
+                class="el-upload-list__item-delete"
+                @click="handleDownload(file)"
+              >
+                <i class="el-icon-download"></i>
+              </span>
+              <span
+                v-if="!disabled"
+                class="el-upload-list__item-delete"
+                @click="handleRemove(file)"
+              >
+                <i class="el-icon-delete"></i>
+              </span>
+            </span>
+          </div>
+        </el-upload>
+        <el-dialog :visible.sync="dialogVisible">
+          <img width="100%" :src="dialogImageUrl" alt="" />
+        </el-dialog>
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取消</el-button>
+      <el-button type="primary" @click="dataFormSubmit()">确定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+import Vue from "vue";
+export default {
+  data() {
+    return {
+      visible: false,
+      dataForm: {
+        id: 0,
+        projectId: "",
+        projectName: "",
+        name: "",
+        memo: "",
+        path: "",
+      },
+      dataRule: {
+        projectName: [
+          { required: true, message: "项目不能为空", trigger: "blur" },
+        ],
+        name: [
+          { required: true, message: "图片名称不能为空", trigger: "blur" },
+        ],
+        memo: [
+          { required: true, message: "图片说明不能为空", trigger: "blur" },
+        ],
+        path: [
+          { required: true, message: "图片路径不能为空", trigger: "blur" },
+        ],
+      },
+      projects: [],
+      headers: { token: Vue.cookie.get("token"), withCredentials: true },
+      data: { dirId: "houseLayout" },
+      uploadUrl: `${window.SITE_CONFIG.baseUrl}/busi/busihouselayout/upload`,
+      dialogImageUrl: "",
+      dialogVisible: false,
+      disabled: false,
+    };
+  },
+  methods: {
+    init(id) {
+      this.dataForm.id = id || 0;
+      this.visible = true;
+      this.$nextTick(() => {
+        this.$refs["dataForm"].resetFields();
+        if (this.dataForm.id) {
+          this.$http({
+            url: this.$http.adornUrl(
+              `/busi/busihouselayout/info/${this.dataForm.id}`
+            ),
+            method: "get",
+            params: this.$http.adornParams(),
+          }).then(({ data }) => {
+            if (data && data.code === 0) {
+              this.dataForm.projectId = data.busiHouseLayout.projectId;
+              this.dataForm.name = data.busiHouseLayout.name;
+              this.dataForm.memo = data.busiHouseLayout.memo;
+              this.dataForm.path = data.busiHouseLayout.path;
+            }
+          });
+        }
+        this.queryProjects();
+      });
+    },
+    // 表单提交
+    dataFormSubmit() {
+      this.$refs["dataForm"].validate((valid) => {
+        if (valid) {
+          this.$http({
+            url: this.$http.adornUrl(
+              `/busi/busihouselayout/${!this.dataForm.id ? "save" : "update"}`
+            ),
+            method: "post",
+            data: this.$http.adornData({
+              id: this.dataForm.id || undefined,
+              projectId: this.dataForm.projectId,
+              name: this.dataForm.name,
+              memo: this.dataForm.memo,
+              path: this.dataForm.path,
+            }),
+          }).then(({ data }) => {
+            if (data && data.code === 0) {
+              this.$message({
+                message: "操作成功",
+                type: "success",
+                duration: 1500,
+                onClose: () => {
+                  this.visible = false;
+                  this.$emit("refreshDataList");
+                },
+              });
+            } else {
+              this.$message.error(data.msg);
+            }
+          });
+        }
+      });
+    },
+    queryProjects() {
+      this.$http.get("/busi/busiproject/list", {
+        page: 0,
+        limit: 1000,
+        success: (data) => {
+          this.projects = data.page.list;
+        },
+      });
+    },
+    listProject(queryString, cb) {
+      let results = this.projects.filter((p) => {
+        return p.name.indexOf(queryString) >= 0;
+      });
+      cb(results);
+    },
+    handleSelect(item) {
+      this.dataForm.proejctId = item.id;
+      this.dataForm.projectName = item.name;
+    },
+    handleIconClick(ev) {
+      console.log(ev);
+    },
+    // 文件上传相关
+    handleRemove(file) {
+      console.log(file);
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    },
+    handleDownload(file) {
+      console.log(file);
+    },
+    hanldeSuccess(response, file, fileList) {
+      console.log(response);
+      console.log(file);
+      console.log(fileList);
+    },
+  },
+  mounted() {
+    this.queryProjects();
+  },
+};
+</script>
+<style lang="scss">
+.el-autocomplete {
+  width: 100%;
+}
+</style>
