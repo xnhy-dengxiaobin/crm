b32fef2999d799f9700234f5bca3b262638d315b
刘辉 <hui.liu@rivo-robot.com>
2020-12-29 18:58:35 +0800

    aa

--- static/config/index-prod.js
+++ static/config/index-prod.js
@@ -5,7 +5,7 @@
   window.SITE_CONFIG = {};
 
   // api接口请求地址
-  window.SITE_CONFIG["baseUrl"] = "http://222.210.108.61:8888/crm-server";
+  window.SITE_CONFIG["baseUrl"] = "https://scmczd.net/crm";
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG["domain"] = "./"; // 域名
